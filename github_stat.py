55e83eb5515ca3ce073fa5cb56da8c4635c328f6
biswas <subrata.biswwas@gmail.com>
2023-02-13 20:11:04 -0500

    github stat added.

--- github_stat.py
+++ github_stat.py
@@ -4,6 +4,7 @@ import json
 import random
 import numpy as np
 from tqdm import tqdm
+from prettytable import PrettyTable
 import matplotlib.pyplot as plt
 
 random.seed(42)
@@ -18,8 +19,8 @@ class GitStats:
         self.val_step = val_step
         self.all_bins = list(range(0, self.max_github_id, self.bin_size))
         self.save_dir = "./saved_json/"
-        self.active_percentage = None
-        self.active_std = None
+        self.active_percentages = []
+        self.active_stds = []
 
     def crawl_data(self, select_bin):
         selected_bins = random.sample(self.all_bins, select_bin)
@@ -40,19 +41,27 @@ class GitStats:
         outfile.close()
 
     def estimate_active_account(self):
-        json_files = os.listdir(self.save_dir)
-        active_pcts = []
+        json_files = sorted(os.listdir(self.save_dir))
+        table = PrettyTable(["# of bins", "mean", "std", "# of active account"])
         for json_file in json_files:
+            active_pcts = []
+            row = [json_file.split(".")[0].split("_")[-1]]
             with open(os.path.join(self.save_dir, json_file)) as file:
                 data = json.load(file)
             file.close()
             for key, value in data.items():
                 active_pcts = active_pcts + value
-        mean = np.mean(active_pcts)
-        std = np.std(active_pcts)
-        self.active_percentage = mean
-        self.active_std = std
-        print(f'# Active Github users: {int(mean*self.max_github_id)} with standard deviation of {round(std, 4)}')
+            mean = round(np.mean(active_pcts), 4)
+            std = round(np.std(active_pcts), 4)
+            row.append(mean)
+            row.append(std)
+            row.append(int(mean*self.max_validation_id))
+            table.add_row(row)
+            self.active_percentages.append(mean)
+            self.active_stds.append(std)
+        print("\t Result on entire ID space")
+        print(table)
+        print("\n")
 
     def validate_result(self, fold=5):
         active_pcts = []
@@ -71,7 +80,7 @@ class GitStats:
             active_pcts.append(1 - in_active_count/self.val_step)
         ticks = list(range(1, len(active_pcts) + 1))
         plt.figure()
-        plt.axhline(y=self.active_percentage, color='green', linewidth=2, label='Predicted active percentage')
+        plt.axhline(y=self.active_percentages[2], color='green', linewidth=2, label='Predicted active percentage')
         plt.plot(ticks, active_pcts, color='blue', linestyle='--', label='Actual active percentage')
         plt.xticks(ticks)
         plt.xlim(ticks[0], ticks[-1])
@@ -114,7 +123,7 @@ def main():
         git_stat.crawl_data(select_bin=select_bin)
     git_stat.estimate_active_account()
     git_stat.plot_result()
-    git_stat.validate_result()
+    # git_stat.validate_result()
     plt.show()
 
 
